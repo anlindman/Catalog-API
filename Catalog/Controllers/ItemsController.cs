533686206a7c5046c33e492025c9cad4e22b771c
anlindman <lindman@post.com>
2022-01-05 13:14:51 +0100

    Add project files.

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers.Models
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
---
+++ Catalog/Repositories/IItemsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Entities;
+using System.Collections.Generic;
+using System;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+}
